05eb4087a5a3150ea69e75ff74bfe568465ba2e7
Fathur Radhy <radhsyn83@Fathurs-MacBook-Pro.local>
2019-05-04 11:58:51 +0700

    add koneksi

--- koneksi.php
+++ koneksi.php
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "radhsyn83@radhsyn", "pwd" => "{your_password_here}", "Database" => "radhsyn83-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "radhsyn83@radhsyn", "pwd" => "Sense324", "Database" => "radhsyn83-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:radhsyn.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
