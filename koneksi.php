9fcce767e02b49577608f1ee738ff28d9ddd6e9f
Fathur Radhy <radhsyn83@Fathurs-MacBook-Pro.local>
2019-05-04 12:10:13 +0700

    add koneksi

--- koneksi.php
+++ koneksi.php
@@ -3,20 +3,22 @@
     try {
         $conn = new PDO("sqlsrv:server = tcp:radhsyn.database.windows.net,1433; Database = radhsyn83-db", "radhsyn83", "Sense324");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+        $stmt = $conn->query("SELECT * FROM register");
+        while ($row = $stmt->fetch()) {
+            echo $row['first_name']."<br />\n";
+        }
     }
     catch (PDOException $e) {
         print("Error connecting to SQL Server.");
         die(print_r($e));
     }
 
-    // SQL Server Extension Sample Code:
-    $connectionInfo = array("UID" => "radhsyn83@radhsyn", "pwd" => "Sense324", "Database" => "radhsyn83-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-    $serverName = "tcp:radhsyn.database.windows.net,1433";
-    $conn = sqlsrv_connect($serverName, $connectionInfo);
+    // // SQL Server Extension Sample Code:
+    // $connectionInfo = array("UID" => "radhsyn83@radhsyn", "pwd" => "Sense324", "Database" => "radhsyn83-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    // $serverName = "tcp:radhsyn.database.windows.net,1433";
+    // $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-    $stmt = $conn->query("SELECT * FROM register");
-    while ($row = $stmt->fetch()) {
-        echo $row['first_name']."<br />\n";
-    }
 
 ?>
