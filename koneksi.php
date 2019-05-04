a1b21d52261d18f0cc28659a02a6cfa9f63332e6
Fathur Radhy <radhsyn83@Fathurs-MacBook-Pro.local>
2019-05-04 11:59:26 +0700

    add koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,17 +1,17 @@
 <?php
     // PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:radhsyn.database.windows.net,1433; Database = radhsyn83-db", "radhsyn83", "{your_password_here}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+    try {
+        $conn = new PDO("sqlsrv:server = tcp:radhsyn.database.windows.net,1433; Database = radhsyn83-db", "radhsyn83", "Sense324");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $e) {
+        print("Error connecting to SQL Server.");
+        die(print_r($e));
+    }
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "radhsyn83@radhsyn", "pwd" => "Sense324", "Database" => "radhsyn83-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:radhsyn.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+    // SQL Server Extension Sample Code:
+    $connectionInfo = array("UID" => "radhsyn83@radhsyn", "pwd" => "Sense324", "Database" => "radhsyn83-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $serverName = "tcp:radhsyn.database.windows.net,1433";
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 ?>
