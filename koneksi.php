e3554703077763ca0f3c993776b7b5b6cde63b8b
Fathur Radhy <radhsyn83@Fathurs-MacBook-Pro.local>
2019-05-04 11:50:09 +0700

    add koneksi

--- koneksi.php
+++ koneksi.php
@@ -2,13 +2,19 @@
     $servername = "radhsyn.database.windows.net";
     $username = "radhsyn83";
     $password = "Sense324";
+    $db = "radhsyn83-db";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password);
+    $link = mysqli_connect($servername, $username, $password, $db);
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } 
-    echo "Connected successfully";
+    if (!$link) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+
+    echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+    echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+    mysqli_close($link);
 ?>
